229967fb94d96ced1285bc33026b63aa6a88a12d
puiuld <puiuld47@gmail.com>
2024-12-04 09:45:49 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Создать метод, который будет вычислять площадь круга и длину окружности. 
+        /// Вызывающий программный код должен передавать в качестве аргумента методу значение радиуса и печатать значение радиуса, площади круга и длины окружности
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int r = Vvod("r=");
+            double p = 1, s = 1;
+            Func(r, ref p, ref s);
+            Console.WriteLine($"Длина окружности= {p}");
+            Console.WriteLine($"Площадь окружности= {s}");
+
+            Console.ReadLine();
+        }
+        public static int Vvod(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Func(int r,ref double p,ref double s)
+        {
+            p = 2 * Math.PI * r;
+            s = Math.PI * Math.Pow(r,2);
+        }
+
+    }
+}
